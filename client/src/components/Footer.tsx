28a490c1dbcc81b5ed97adb3baf61b79e38814d1
yura4ka <yura4ka05@gmail.com>
2024-01-07 22:30:05 +0200

    feat: add footer

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -1,5 +1,11 @@
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className="border-t py-4">
+      <div className="container text-center text-sm text-muted-foreground">
+        Copyright &copy; 2024. All rights reserved
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
